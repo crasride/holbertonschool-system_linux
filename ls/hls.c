b241b4f9cebeb26484e4e41d97aa433170751c4a
crasride <jose.dfernandez@yahoo.fr>
2023-07-24 17:22:53 +0200

    Maybe some parameters v.1.2

--- ls/hls.c
+++ ls/hls.c
@@ -18,57 +18,56 @@ int i;
 
 if (argc == 1)
 {
-/** No arguments supplied, list the contents of the current directory */
+// No arguments supplied, list the contents of the current directory
 dir = opendir(".");
 if (dir == NULL)
 {
-perror("opendir");
-return (1);
+    perror("opendir");
+    return 1;
 }
 while ((ent = readdir(dir)) != NULL)
 {
-if (ent->d_name[0] != '.')
-{
-printf("%s  ", ent->d_name);
-}
+    if (ent->d_name[0] != '.')
+    {
+        printf("%s  ", ent->d_name);
+    }
 }
 printf("\n");
 closedir(dir);
 }
 else
 {
-/** List contents of directories or files given as arguments */
+// List contents of directories or files given as arguments
 for (i = 1; i < argc; i++)
 {
-dir = opendir(argv[i]);
-if (dir != NULL)
-{
-printf("%s:\n", argv[i]);
-while ((ent = readdir(dir)) != NULL)
-{
-if (ent->d_name[0] != '.')
-{
-printf("%s  ", ent->d_name);
+    dir = opendir(argv[i]);
+    if (dir != NULL)
+    {
+        printf("%s:\n", argv[i]);
+        while ((ent = readdir(dir)) != NULL)
+        {
+            if (ent->d_name[0] != '.')
+            {
+                printf("%s  ", ent->d_name);
+            }
+        }
+        printf("\n");
+        closedir(dir);
+    }
+    else
+    {
+        struct stat st;
+        if (stat(argv[i], &st) == 0 && S_ISREG(st.st_mode))
+        {
+            printf("%s\n", argv[i]);
+        }
+        else
+        {
+            fprintf(stderr, "%s: cannot access %s: No such file or directory\n", argv[0], argv[i]);
+            return 1;
+        }
+    }
 }
 }
-printf("\n");
-closedir(dir);
-}
-else
-{
-struct stat st;
-if (stat(argv[i], &st) == 0 && S_ISREG(st.st_mode))
-{
-printf("%s\n", argv[i]);
-}
-else
-{
-fprintf(stderr, "%s: cannot access %s: No such file or directory\n", argv[0],
-argv[i]);
-return (1);
-}
-}
-}
-}
-return (0);
+return 0;
 }
