41444173268bb57ba43fb522f8eb9a6169e67f3a
crasride <jose.dfernandez@yahoo.fr>
2023-07-25 13:16:03 +0200

    add On a non existing folder

--- ls/hls.c
+++ ls/hls.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 
 /**
  * list_files - Function that lists the files in a directory excluding hidden
@@ -14,26 +15,28 @@
 
 
 void list_files(const char *path)
-{
+	{
 	DIR *dir;
 	struct dirent *ent;
 
+	// Attempt to open the path using opendir
 	dir = opendir(path);
-	if (dir == NULL)
+	if (dir != NULL)
 	{
-
-		printf("%s\n", path);
-		return;
-	}
-
-
-	while ((ent = readdir(dir)) != NULL)
-	{
-		if (ent->d_name[0] != '.')
+		// It's a directory, proceed with listing files
+		while ((ent = readdir(dir)) != NULL)
 		{
-			printf("%s  ", ent->d_name);
+			if (ent->d_name[0] != '.')
+			{
+				printf("%s  ", ent->d_name);
+			}
 		}
+		printf("\n");
+		closedir(dir);
 	}
-	printf("\n");
-	closedir(dir);
-}
+	else
+	{
+		// Print the error message to stderr
+		fprintf(stderr, "./hls_01: cannot access %s: %s\n", path, strerror(errno));
+	}
+	}
