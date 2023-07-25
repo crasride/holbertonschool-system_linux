1550c5905981d776de28243f6a9e5896bd88d195
crasride <jose.dfernandez@yahoo.fr>
2023-07-25 12:53:39 +0200

    error

--- ls/hls.c
+++ ls/hls.c
@@ -3,6 +3,8 @@
 #include <dirent.h>
 #include "hls.h"
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
  * list_files - Function that lists the files in a directory excluding hidden
@@ -10,44 +12,28 @@
  * @path: The path of the directory to list.
  */
 
-#include <sys/stat.h>
 
 void list_files(const char *path)
 {
-	struct stat path_stat;
 	DIR *dir;
 	struct dirent *ent;
 
-	if (stat(path, &path_stat) != 0)
+	dir = opendir(path);
+	if (dir == NULL)
 	{
-		/* File/directory doesn't exist or inaccessible */
-		fprintf(stderr, "./hls_01: cannot access %s: No such file or directory\n", path);
+
+		printf("%s\n", path);
 		return;
 	}
 
-	if (S_ISDIR(path_stat.st_mode))
-	{
-		/* It's a directory, proceed with listing files */
-		dir = opendir(path);
-		if (dir == NULL)
-		{
-			fprintf(stderr, "./hls_01: cannot access %s: No such file or directory\n", path);
-			return;
-		}
 
-		while ((ent = readdir(dir)) != NULL)
-		{
-			if (ent->d_name[0] != '.')
-			{
-				printf("%s  ", ent->d_name);
-			}
-		}
-		printf("\n");
-		closedir(dir);
-	}
-	else
+	while ((ent = readdir(dir)) != NULL)
 	{
-		/* It's a file, just print the file name */
-		printf("%s\n", path);
+		if (ent->d_name[0] != '.')
+		{
+			printf("%s  ", ent->d_name);
+		}
 	}
+	printf("\n");
+	closedir(dir);
 }
