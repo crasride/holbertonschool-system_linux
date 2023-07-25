38cb031e373f9a7d29e03d0a49934f8ca9a58bce
crasride <jose.dfernandez@yahoo.fr>
2023-07-25 12:46:05 +0200

    error 2

--- ls/hls.c
+++ ls/hls.c
@@ -10,25 +10,44 @@
  * @path: The path of the directory to list.
  */
 
+#include <sys/stat.h>
+
 void list_files(const char *path)
-	{
+{
+	struct stat path_stat;
 	DIR *dir;
 	struct dirent *ent;
 
-	dir = opendir(path);
-	if (dir == NULL)
+	if (stat(path, &path_stat) != 0)
 	{
+		// File/directory doesn't exist or inaccessible
 		fprintf(stderr, "./hls_01: cannot access %s: No such file or directory\n", path);
-		exit(1);
+		return;
 	}
 
-	while ((ent = readdir(dir)) != NULL)
+	if (S_ISDIR(path_stat.st_mode))
 	{
-		if (ent->d_name[0] != '.')
+		// It's a directory, proceed with listing files
+		dir = opendir(path);
+		if (dir == NULL)
 		{
-			printf("%s  ", ent->d_name);
+			fprintf(stderr, "./hls_01: cannot access %s: No such file or directory\n", path);
+			return;
 		}
+
+		while ((ent = readdir(dir)) != NULL)
+		{
+			if (ent->d_name[0] != '.')
+			{
+				printf("%s  ", ent->d_name);
+			}
+		}
+		printf("\n");
+		closedir(dir);
 	}
-	printf("\n");
-	closedir(dir);
+	else
+	{
+		// It's a file, just print the file name
+		printf("%s\n", path);
+	}
 }
