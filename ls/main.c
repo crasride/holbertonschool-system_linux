55d7fb6d2145540642f78f77bd8080c0ff3fed74
crasride <jose.dfernandez@yahoo.fr>
2023-07-26 14:28:30 +0200

    task 2

--- ls/main.c
+++ ls/main.c
@@ -14,49 +14,49 @@
 
 int main(int argc, char *argv[])
 {
-    int one_option = 0;
-    int i, j;
+	int one_option = 0;
+	int i, j;
 
-    /* Buscamos la opción -1 en los argumentos */
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (argv[i][j] != '-' || argv[i][j + 1] != '1' || argv[i][j + 2] != '\0')
-            {
-                break; // No es la opción -1
-            }
-            one_option = 1;
-            break;
-        }
-    }
+	/* Buscamos la opción -1 en los argumentos */
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] != '-' || argv[i][j + 1] != '1' || argv[i][j + 2] != '\0')
+			{
+				break;
+			}
+			one_option = 1;
+			break;
+		}
+	}
 
-    if (argc == 1 || (argc == 2 && one_option))
-    {
-        list_files(".", argv[0], argc, one_option);
-    }
-    else
-    {
-        for (i = 1; i < argc; i++)
-        {
-            /* Saltamos la opción -1 */
-            for (j = 0; argv[i][j] != '\0'; j++)
-            {
-                if (argv[i][j] != '-' || argv[i][j + 1] != '1' || argv[i][j + 2] != '\0')
-                {
-                    break;
-                }
-                continue;
-            }
+	if (argc == 1 || (argc == 2 && one_option))
+	{
+		list_files(".", argv[0], argc, one_option);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			/* Saltamos la opción -1 */
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] != '-' || argv[i][j + 1] != '1' || argv[i][j + 2] != '\0')
+				{
+					break;
+				}
+				continue;
+			}
 
-            list_files(argv[i], argv[0], argc, one_option);
+			list_files(argv[i], argv[0], argc, one_option);
 
-            if (i < argc - 1)
-            {
-                printf("\n");
-            }
-        }
-    }
-    return 0;
+			if (i < argc - 1)
+			{
+				printf("\n");
+			}
+		}
+	}
+	return 0;
 }
 
