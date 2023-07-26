404e9b7b5940b8a1b8a1dee7aeacf534daaf11a6
crasride <jose.dfernandez@yahoo.fr>
2023-07-26 12:10:32 +0200

    last chechk task 1

--- ls/hls.c
+++ ls/hls.c
@@ -35,7 +35,7 @@ void list_files(const char *path, const char *program_name, int num_args)
 	{
 		if (errno == EACCES)
 		{
-			fprintf(stderr, "%s: cannot open directory %s: Permission denied", program_name, path);
+			fprintf(stderr, "%s: cannot open directory %s: ", program_name, path);
 			perror("");
 			exit(EXIT_FAILURE);
 		}
