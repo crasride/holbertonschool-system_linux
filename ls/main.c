dd497bfd3bd1e4da1eddfe92e392ddb6cf79407f
crasride <jose.dfernandez@yahoo.fr>
2023-07-27 12:33:22 +0200

    update

--- ls/main.c
+++ ls/main.c
@@ -12,24 +12,35 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int display_one_per_line = 0;
-	int j;
 
 
 	/* Check for the "-1" option in the command-line arguments */
-	for (i = 1; i < argc; i++)
-	{
-		if (argv[i][0] == '-' && argv[i][1] == '1' && argv[i][2] == '\0')
-		{
-			display_one_per_line = 1;
-			/* Remove the "-1" option from the arguments */
-			for (j = i; j < argc - 1; j++)
-			{
-				argv[j] = argv[j + 1];
-			}
-			argc--; /* Decrement the argument count to exclude the "-1" option*/
-			i--; /* Adjust the index to review the current position again */
-		}
-	}
+    for (i = 1; i < argc; i++)
+    {
+        int j;
+        int is_option_1 = 1;
+
+        for (j = 0; argv[i][j] != '\0'; j++)
+        {
+            if (argv[i][j] != '-' && argv[i][j] != '1')
+            {
+                is_option_1 = 0;
+                break;
+            }
+        }
+
+        if (is_option_1)
+        {
+            display_one_per_line = 1;
+            // Remove the "-1" option from the arguments
+            for (int k = i; k < argc - 1; k++)
+            {
+                argv[k] = argv[k + 1];
+            }
+            argc--; // Decrement the argument count to exclude the "-1" option
+            i--;    // Adjust the index to review the current position again
+        }
+    }
 
 	/* Process the files and directories passed as arguments */
 	if (argc == 1)
