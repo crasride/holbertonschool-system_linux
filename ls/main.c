7de053dfe645735081850c2cd26905738a1bce21
crasride <jose.dfernandez@yahoo.fr>
2023-07-27 09:58:57 +0200

    task2

--- ls/main.c
+++ ls/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "hls.h"
+#include <string.h>
 
 /**
  * main - Entry point of the program.
@@ -12,22 +13,32 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int display_one_per_line = 0;
-	int j;
-
 
 	/* Check for the "-1" option in the command-line arguments */
 	for (i = 1; i < argc; i++)
 	{
-		if (argv[i][0] == '-' && argv[i][1] == '1' && argv[i][2] == '\0')
+		if (argv[i][0] == '-')
 		{
-			display_one_per_line = 1;
-			/* Remove the "-1" option from the arguments */
-			for (j = i; j < argc - 1; j++)
+			int num_ones = 0;
+			int j = 1;
+
+			while (argv[i][j] == '1')
 			{
-				argv[j] = argv[j + 1];
+				num_ones++;
+				j++;
 			}
-			argc--; /* Decrement the argument count to exclude the "-1" option*/
-			i--; /* Adjust the index to review the current position again */
+
+			if (num_ones > 0 && argv[i][j] == '\0')
+			{
+				display_one_per_line = 1;
+				/* Remove the "-1" option from the arguments */
+				for (; j < argc - 1; j++)
+				{
+					argv[j] = argv[j + 1];
+				}
+				argc--; /* Decrement the argument count to exclude the "-1" option*/
+				i--; /* Adjust the index to review the current position again */
+			}
 		}
 	}
 
@@ -41,7 +52,7 @@ int main(int argc, char *argv[])
 		for (i = 1; i < argc; i++)
 		{
 			list_files(argv[i], argv[0], argc, display_one_per_line);
-			if (i < argc - 1)
+			if (i < argc - 1 && display_one_per_line)
 			{
 				printf("\n");
 			}
@@ -49,4 +60,3 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
-
