24c2dc5685b9e86296ee4f374ab86bbff27ca54e
crasride <jose.dfernandez@yahoo.fr>
2023-07-27 10:01:06 +0200

    task2

--- ls/main.c
+++ ls/main.c
@@ -14,33 +14,7 @@ int main(int argc, char *argv[])
 	int i;
 	int display_one_per_line = 0;
 
-	/* Check for the "-1" option in the command-line arguments */
-	for (i = 1; i < argc; i++)
-	{
-		if (argv[i][0] == '-')
-		{
-			int num_ones = 0;
-			int j = 1;
-
-			while (argv[i][j] == '1')
-			{
-				num_ones++;
-				j++;
-			}
-
-			if (num_ones > 0 && argv[i][j] == '\0')
-			{
-				display_one_per_line = 1;
-				/* Remove the "-1" option from the arguments */
-				for (; j < argc - 1; j++)
-				{
-					argv[j] = argv[j + 1];
-				}
-				argc--; /* Decrement the argument count to exclude the "-1" option*/
-				i--; /* Adjust the index to review the current position again */
-			}
-		}
-	}
+	
 
 	/* Process the files and directories passed as arguments */
 	if (argc == 1)
