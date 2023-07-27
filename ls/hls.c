d1eb58f7ad856c22701bdd157e96b89b7dd2fc2a
crasride <jose.dfernandez@yahoo.fr>
2023-07-27 10:58:27 +0200

    function srtcpy

--- ls/hls.c
+++ ls/hls.c
@@ -8,15 +8,35 @@
 #include <errno.h>
 
 /**
+ * my_strcpy - Function to copy a string from source to destination.
+ * @dest: The destination buffer.
+ * @src: The source string to copy.
+ * Return: Pointer to the destination buffer.
+ */
+char *my_strcpy(char *dest, const char *src)
+{
+	char *dest_ptr = dest;
+
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+
+	return (dest_ptr);
+}
+
+/**
  * list_files - Function that lists the files in a directory excluding hidden
  * ones (those starting with '.').
  * @path: The path of the directory to list.
  * @program_name: The name of the program (argv[0]).
- * @num_args: The number of arguments
- * @display_one_per_line: Whether
+ * @num_args: The number of arguments.
+ * @display_one_per_line: Whether to display one file per line.
  */
-
-
 void list_files(const char *path, const char *program_name, int num_args, int display_one_per_line)
 {
 	DIR *dir;
@@ -34,18 +54,15 @@ void list_files(const char *path, const char *program_name, int num_args, int di
 
 	dir = opendir(path);
 	if (dir == NULL)
-
 	{
 		if (errno == EACCES)
 		{
-			fprintf(stderr, "%s: cannot open directory %s: ", program_name, path);
-			perror("");
+			perror(program_name);
 			exit(EXIT_FAILURE);
 		}
 		else
 		{
-			fprintf(stderr, "%s: cannot access %s: ", program_name, path);
-			perror("");
+			perror(program_name);
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -59,8 +76,12 @@ void list_files(const char *path, const char *program_name, int num_args, int di
 	{
 		if (ent->d_name[0] != '.')
 		{
-			snprintf(files[num_files], sizeof(files[num_files]), "%s", ent->d_name);
-			num_files++;
+			size_t name_length = strlen(ent->d_name);
+			if (name_length < sizeof(files[num_files]) - 1)
+			{
+				my_strcpy(files[num_files], ent->d_name);
+				num_files++;
+			}
 		}
 	}
 	closedir(dir);
