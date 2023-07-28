6ee955dd9f00e1bb6f905a25815c0b8384e562ba
crasride <jose.dfernandez@yahoo.fr>
2023-07-28 15:38:34 +0200

    update

--- ls/hls.c
+++ ls/hls.c
@@ -24,7 +24,6 @@ char *my_strcpy(char *dest, const char *src)
 }
 
 
-
 /**
  * list_files - Function that lists the files in a directory excluding hidden
  * ones (those starting with '.').
@@ -36,76 +35,73 @@ char *my_strcpy(char *dest, const char *src)
  */
 void list_files(const char *path, const char *program_name, int num_args, int display_one_per_line, struct EntryList *list)
 {
-    DIR *dir;
-    struct dirent *ent;
-    struct stat file_stat;
+	DIR *dir;
+	struct dirent *ent;
 
-    if (lstat(path, &file_stat) == 0 && S_ISREG(file_stat.st_mode))
-    {
-        printf("%s\n", path);
-        return;
-    }
+	struct stat file_stat;
+	if (lstat(path, &file_stat) == 0 && S_ISREG(file_stat.st_mode))
+	{
+		printf("%s\n", path);
+		return;
+	}
 
-    dir = opendir(path);
-    if (dir == NULL)
-    {
-        if (errno == EACCES)
-        {
-            fprintf(stderr, "%s: cannot open directory %s: ", program_name, path);
-            perror("");
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            fprintf(stderr, "%s: cannot access %s: ", program_name, path);
-            perror("");
-            exit(EXIT_FAILURE);
-        }
-    }
+	dir = opendir(path);
+	if (dir == NULL)
+	{
+		if (errno == EACCES)
+		{
+			fprintf(stderr, "%s: cannot open directory %s: ", program_name, path);
+			perror("");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			fprintf(stderr, "%s: cannot access %s: ", program_name, path);
+			perror("");
+			exit(EXIT_FAILURE);
+		}
+	}
 
-    if (num_args > 2)
-    {
-        printf("%s:\n", path);
-    }
+	if (num_args > 2)
+	{
+		printf("%s:\n", path);
+	}
 
-    struct Entry *current;
+	while ((ent = readdir(dir)) != NULL)
+	{
+		if (ent->d_name[0] != '.')
+		{
+			add_entry_to_list(list, ent->d_name, file_stat.st_mode);
+		}
+	}
+	closedir(dir);
 
-    while ((ent = readdir(dir)) != NULL)
-    {
-        if (ent->d_name[0] != '.')
-        {
-            add_entry_to_list(list, ent->d_name, file_stat.st_mode);
-        }
-    }
+	if (list->count == 0)
+	{
+		fprintf(stderr, "%s: %s: No such file or directory\n", program_name, path);
+		exit(EXIT_FAILURE);
+	}
 
-    closedir(dir);
+	struct Entry *current = list->head;
 
-    if (list->count == 0)
-    {
-        fprintf(stderr, "%s: %s: No such file or directory\n", program_name, path);
-        exit(EXIT_FAILURE);
-    }
-
-    current = list->head;
-
-    if (display_one_per_line)
-    {
-        while (current != NULL)
-        {
-            printf("%s\n", current->name);
-            current = current->next;
-        }
-        printf("\n");
-    }
-    else
-    {
-        while (current != NULL)
-        {
-            printf("%s  ", current->name);
-            current = current->next;
-        }
-        printf("\n");
-    }
+	if (display_one_per_line)
+	{
+		while (current != NULL)
+		{
+			printf("%s\n", current->name);
+			current = current->next;
+		}
+		printf("\n");
+	}
+	else
+	{
+		while (current != NULL)
+		{
+			printf("%s  ", current->name);
+			current = current->next;
+		}
+		printf("\n");
+	}
 }
 
 /**
