441287d07eb584922d54314eedb064260cc4bf3a
crasride <jose.dfernandez@yahoo.fr>
2023-07-30 11:40:28 +0200

    task 4

--- ls/main.c
+++ ls/main.c
@@ -1,66 +1,79 @@
-	#include <stdio.h>
-	#include <stdlib.h>
-	#include "hls.h"
-	#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "hls.h"
+#include <string.h>
 
-	/**
-	 * main - Entry point of the program.
-	 * @argc: Number of command-line arguments.
-	 * @argv: Array of command-line argument strings.
-	 * Return: 0 on success, non-zero on error.
-	 */
+/**
+ * main - Entry point of the program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ * Return: 0 on success, non-zero on error.
+ */
 
-	int my_strcmp(const char *str1, const char *str2) {
-	while (*str1 != '\0' && *str1 == *str2) {
-		str1++;
-		str2++;
-	}
-	return *str1 - *str2;
-	}
+int main(int argc, char *argv[])
+{
+int i, k, j;
+int display_one_per_line = 0;
+int show_hidden = 0; /* Flag to indicate if hidden files should be shown */
+int show_almost_all = 0;
 
-	int main(int argc, char *argv[])
+/* Check for the "-1" and "-a" options in the command-line arguments */
+for (i = 1; i < argc; i++)
+{
+int is_option_1 = 1;
+int is_option_a = 1;
+int is_option_A = 1;
+
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+	if (argv[i][j] != '-' && argv[i][j] != '1')
 	{
-	int i, k;
-	int display_one_per_line = 0;
-	int show_hidden = 0;
-	int show_almost_all = 0;
-
-	for (i = 1; i < argc; i++)
-	{
-		if (my_strcmp(argv[i], "-1") == 0)
-		{
-			display_one_per_line = 1;
-			for (k = i; k < argc - 1; k++)
-			{
-				argv[k] = argv[k + 1];
-			}
-			argc--;
-			i--;
-		}
-		else if (my_strcmp(argv[i], "-a") == 0)
-		{
-			show_hidden = 1;
-			for (k = i; k < argc - 1; k++)
-			{
-				argv[k] = argv[k + 1];
-			}
-			argc--;
-			i--;
-		}
-		else if (my_strcmp(argv[i], "-A") == 0)
-		{
-			show_almost_all = 1;
-			for (k = i; k < argc - 1; k++)
-			{
-				argv[k] = argv[k + 1];
-			}
-			argc--;
-			i--;
-		}
+		is_option_1 = 0;
 	}
+	if (argv[i][j] != '-' && argv[i][j] != 'a')
+	{
+		is_option_a = 0;
+	}
+	if (argv[i][j] != '-' && argv[i][j] != 'A')
+	{
+		is_option_A = 0;
+	}
+}
+
+if (is_option_1)
+{
+	display_one_per_line = 1;
+	for (k = i; k < argc - 1; k++)
+	{
+		argv[k] = argv[k + 1];
+	}
+	argc--;
+	i--;
+}
+else if (is_option_a)
+{
+	show_hidden = 1;
+	for (k = i; k < argc - 1; k++)
+	{
+		argv[k] = argv[k + 1];
+	}
+	argc--;
+	i--;
+}
+else if (is_option_A)
+{
+	show_almost_all = 1;
+	for (k = i; k < argc - 1; k++)
+	{
+		argv[k] = argv[k + 1];
+	}
+	argc--;
+	i--;
+}
+}
 
 
-	/* Process the files and directories passed as arguments */
+/* Process the files and directories passed as arguments */
 	if (argc == 1)
 	{
 		struct EntryList list;
@@ -93,4 +106,3 @@
 	}
 	return (0);
 	}
-
