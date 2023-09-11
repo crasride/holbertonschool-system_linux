02f128776cdbda13d367e15d9de72eb00b7589f1
crasride <jose.dfernandez@yahoo.fr>
2023-09-11 20:54:44 +0200

    update

--- readelf/hreadelf.h
+++ readelf/hreadelf.h
@@ -15,5 +15,11 @@ typedef struct
 	Elf64_Ehdr ehdr;
 } Elf64_Header;
 
+/* Functions 0-hreadelf_tools.c */
+const char *get_osabi_name(uint8_t osabi);
+const char *get_osabi_version(uint8_t version);
+const char *get_osabi_type(uint16_t type);
+const char *get_osabi_machine(uint16_t machine);
+const char *get_osabi_data(uint8_t data);
 
 #endif /* HELF_H */
