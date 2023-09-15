2ecfd323d7cad4a38b3422c8e2d5dd2e5ba513b5
crasride <jose.dfernandez@yahoo.fr>
2023-09-16 00:09:09 +0200

    up

--- readelf/1-hreadelf.c
+++ readelf/1-hreadelf.c
@@ -5,93 +5,102 @@
 
 int main(int argc, char *argv[])
 {
-    FILE *file = NULL;
-    int index = 1; // Cambia el índice inicial a 1
-    Elf32_Ehdr elf_header32;
-    Elf64_Ehdr elf_header64;
-    off_t section_table_offset;
-    Elf32_Shdr section_header32;
-    Elf64_Shdr section_header64;
-    char* SectNames = NULL;
-    int is_32bit = 0; /* Variable para detectar si es un archivo de 32 bits */
-    if (argc != 2) {
-        fprintf(stderr, "Uso: %s <archivo ELF>\n", argv[0]);
-        return 1;
-    }
-    file = fopen(argv[1], "rb");
-    if (file == NULL) {
-        perror("No se puede abrir el archivo");
-        return 1;
-    }
-    /* Leer el encabezado ELF principal */
-    fread(&elf_header32, sizeof(Elf32_Ehdr), 1, file);
-    /* Verificar si es un archivo ELF de 32 bits */
-    if (elf_header32.e_ident[EI_CLASS] == ELFCLASS32)
-    {
-        is_32bit = 1;
-    }
-    else
-    {
-        /* Retroceder al principio del archivo si no es un archivo de 32 bits */
-        fseek(file, 0, SEEK_SET);
-        /* Leer el encabezado ELF de 64 bits */
-        fread(&elf_header64, sizeof(Elf64_Ehdr), 1, file);
-    }
-    /* Leer la posición de la tabla de secciones */
-    section_table_offset = is_32bit ? elf_header32.e_shoff : elf_header64.e_shoff;
-    /* Create sectHdr and SectNames */
-    if (is_32bit)
-    {
-        fseek(file, elf_header32.e_shoff + elf_header32.e_shstrndx * elf_header32.e_shentsize, SEEK_SET);
-        SectNames = get_section_name32(section_header32, file);
-    }
-    else
-    {
-        fseek(file, elf_header64.e_shoff + elf_header64.e_shstrndx * elf_header64.e_shentsize, SEEK_SET);
-        SectNames = get_section_name64(section_header64, file);
-    }
-    /* Imprimir la información sobre la tabla de secciones */
-    printf("There are %u section headers, starting at offset 0x%lx:\n\n",
-        is_32bit ? elf_header32.e_shnum : elf_header64.e_shnum,
-        section_table_offset);
-    /* Ubicar la tabla de secciones */
-    if (is_32bit) {
-        fseek(file, elf_header32.e_shoff, SEEK_SET);
-    }
-    else {
-        fseek(file, elf_header64.e_shoff, SEEK_SET);
-    }
-    /* Leer cada entrada de encabezado de sección */
-    printf("Section Headers:\n");
-    printf("  [Nr] Name              Type            Addr     Off    Size   ES Flg Lk Inf Al\n");
-    if (is_32bit) {
-        Elf32_Shdr section_header32;
-        for (; index <= elf_header32.e_shnum; index++) // Cambia el índice inicial a 1
-        {
-            char* name = "";
-            fread(&section_header32, sizeof(Elf32_Shdr), 1, file);
-            if (section_header32.sh_name)
-                name = SectNames + section_header32.sh_name;
-            /* Llamar a la función para imprimir la información de la sección de 32 bits */
-            print_Section_Info_32bits(index, section_header32, name);
-        }
-    }
-    else
-    {
-        Elf64_Shdr section_header64;
-        for (; index <= elf_header64.e_shnum; index++) // Cambia el índice inicial a 1
-        {
-            char* name = "";
-            fread(&section_header64, sizeof(Elf64_Shdr), 1, file);
-            if (section_header64.sh_name)
-                name = SectNames + section_header64.sh_name;
-            /* Llamar a la función para imprimir la información de la sección de 64 bits */
-            print_Section_Info_64bits(index, section_header64, name);
-        }
-    }
-    printKeyToFlags();
-    fclose(file);
-    return 0;
+	FILE *file = NULL;
+	int index = 0;
+	Elf32_Ehdr elf_header32;
+	Elf64_Ehdr elf_header64;
+	off_t section_table_offset;
+	Elf32_Shdr section_header32;
+	Elf64_Shdr section_header64;
+	char* SectNames = NULL;
+	int is_32bit = 0; /* Variable para detectar si es un archivo de 32 bits */
+	if (argc != 2) {
+		fprintf(stderr, "Uso: %s <archivo ELF>\n", argv[0]);
+		return 1;
+	}
+	file = fopen(argv[1], "rb");
+	if (file == NULL) {
+		perror("No se puede abrir el archivo");
+		return 1;
+	}
+	/* Leer el encabezado ELF principal */
+	fread(&elf_header32, sizeof(Elf32_Ehdr), 1, file);
+	/* Verificar si es un archivo ELF de 32 bits */
+	if (elf_header32.e_ident[EI_CLASS] == ELFCLASS32)
+	{
+		is_32bit = 1;
+	}
+	else
+	{
+		/* Retroceder al principio del archivo si no es un archivo de 32 bits */
+		fseek(file, 0, SEEK_SET);
+		/* Leer el encabezado ELF de 64 bits */
+		fread(&elf_header64, sizeof(Elf64_Ehdr), 1, file);
+	}
+	/* Leer la posición de la tabla de secciones */
+	section_table_offset = is_32bit ? elf_header32.e_shoff : elf_header64.e_shoff;
+	/* Create sectHdr and SectNames */
+	if (is_32bit)
+	{
+		fseek(file, elf_header32.e_shoff + elf_header32.e_shstrndx * elf_header32.e_shentsize, SEEK_SET);
+		SectNames = get_section_name32(section_header32, file);
+	}
+	else
+	{
+		fseek(file, elf_header64.e_shoff + elf_header64.e_shstrndx * elf_header64.e_shentsize, SEEK_SET);
+		SectNames = get_section_name64(section_header64, file);
+	}
+	/* Imprimir la información sobre la tabla de secciones */
+	printf("There are %u section headers, starting at offset 0x%lx:\n\n",
+		is_32bit ? elf_header32.e_shnum : elf_header64.e_shnum,
+		section_table_offset);
+	/* Ubicar la tabla de secciones */
+	if (is_32bit) {
+		fseek(file, elf_header32.e_shoff, SEEK_SET);
+	}
+	else {
+		fseek(file, elf_header64.e_shoff, SEEK_SET);
+	}
+	/* Leer cada entrada de encabezado de sección */
+	printf("Section Headers:\n");
+	printf("  [Nr] Name              Type            Addr     Off    Size   ES Flg Lk Inf Al\n");
+	if (is_32bit) {
+		Elf32_Shdr section_header32;
+		for (index = 0; index < elf_header32.e_shnum; index++)
+		{
+			char* name = "";
+			fread(&section_header32, sizeof(Elf32_Shdr), 1, file);
+			/* fseek(file, elf_header32.e_shoff + index * sizeof(section_header32), SEEK_SET);
+			fread(&section_header32, 1, sizeof(section_header32), file);
+ */
+			if (section_header32.sh_name)
+				name = SectNames + section_header32.sh_name;
+			/* fread(&section_header32, sizeof(Elf32_Shdr), 1, file); */
+			/* Llamar a la función para imprimir la información de la sección de 32 bits */
+			print_Section_Info_32bits(index, section_header32, name);
+		}
+	}
+	else
+	{
+		Elf64_Shdr section_header64;
+		for (index = 0; index < elf_header64.e_shnum; index++)
+		{
+			char* name = "";
+			fread(&section_header64, sizeof(Elf64_Shdr), 1, file);
+
+			/* fseek(file, elf_header64.e_shoff + index * sizeof(section_header64), SEEK_SET);
+			fread(&section_header64, 1, sizeof(section_header64), file); */
+
+			if (section_header64.sh_name)
+				name = SectNames + section_header64.sh_name;
+			/* fread(&section_header64, sizeof(Elf64_Shdr), 1, file); */
+			/* Llamar a la función para imprimir la información de la sección de 64 bits */
+			print_Section_Info_64bits(index, section_header64, name);
+		}
+	}
+	printKeyToFlags();
+	fclose(file);
+	return 0;
 }
 
 
