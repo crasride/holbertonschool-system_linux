39f541302fd1b6aa48c44c4083c066edf17ad4c5
crasride <jose.dfernandez@yahoo.fr>
2023-09-26 11:40:56 +0200

    task 0

---
+++ libasm/0-strlen.asm
@@ -0,0 +1,22 @@
+section .text
+global asm_strlen
+
+asm_strlen:
+	push rbp                ; Save the base pointer
+	mov rbp, rsp            ; Set up the stack frame
+	push rcx                ; Save the counter register
+
+	xor rcx, rcx            ; Initialize rcx to 0 (length counter)
+
+.loop:
+	comp byte [rdi + rcx], 0; Compare the byte at rdi+rcx to the null terminator
+	je .done                ; If it's the null terminator,Ok
+	inc rcx                 ; Move to the next character
+	jmp .loop               ; Repeat the loop
+
+.done:
+	mov rax, rcx            ; Move the length in rcx to rax (return value)
+	pop rcx                 ; Restore counter register
+	mov rsp, rbp            ; Restore stack pointer
+	pop rbp                 ; Restore the base pointer
+	ret                     ; Return with the length in rax
