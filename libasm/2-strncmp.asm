c320c75a53abc5e8f44378f840126b3353661795
crasride <jose.dfernandez@yahoo.fr>
2023-09-26 17:46:12 +0200

    test

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -2,32 +2,38 @@ global asm_strncmp
 section .text
 
 asm_strncmp:
-	push rbp
-	mov rbp, rsp
-	mov rcx, 0
-compare:
-	xor rax, rax
-	xor rbx, rbx
-	cmp ecx, edx
-	je exit
-	mov al, byte [rdi + rcx]
-	mov bl, byte [rsi + rcx]
-	cmp al, bl
-	je equal
-	jl less
-	jg greater
-equal:
-	cmp al, 0
-	je exit
-	inc rcx
-	jmp compare
-less:
-	mov rax, -1
-	jmp exit
-greater:
-	mov rax, 1
-	jmp exit
-exit:
-	mov rsp, rbp
-	pop rbp
-	ret
+    push rbp
+    mov rbp, rsp
+
+    xor rax, rax  ; RAX se usará para devolver el resultado
+    xor rcx, rcx  ; RCX se usará como contador
+
+.compare_loop:
+    mov al, [rdi + rcx]  ; Carga el byte de s1 en AL
+    mov bl, [rsi + rcx]  ; Carga el byte de s2 en BL
+    cmp al, bl           ; Compara los bytes
+    jne .not_equal       ; Si no son iguales, salta a .not_equal
+
+    ; Si ambos bytes son iguales y no hemos llegado a n, continuamos
+    inc rcx
+    cmp rcx, rdx
+    jl .compare_loop
+
+    ; Si llegamos a n o al final de una de las cadenas, son iguales
+    cmp rcx, rdx
+    je .equal
+
+.not_equal:
+    ; Si los bytes son diferentes o uno de los punteros llega al final, determina el resultado
+    mov rax, 1   ; RAX = 1 (mayor)
+    jl .end
+    mov rax, -1  ; RAX = -1 (menor)
+
+.equal:
+    ; Si las cadenas son iguales, RAX ya está configurado en 0
+
+.end:
+    mov rsp, rbp
+    pop rbp
+    ret
+
