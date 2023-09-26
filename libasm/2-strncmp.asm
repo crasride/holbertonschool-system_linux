d7252d3be1adcc4ecd2e31d20bab76f6c09b991d
crasride <jose.dfernandez@yahoo.fr>
2023-09-26 18:58:13 +0200

    add comment code

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -1,48 +1,62 @@
 BITS 64
 
-global asm_strncmp
-section .text
+	global asm_strncmp
+
+	section .text
+	;int asm_strncmp(const char *s1, const char *s2, size_t n);
+	;Compare two character strings (char *) up to a specified maximum number of
+	;characters (n), and returns an integer value indicating the relationship
+	;between the two strings.
+
+
 
 asm_strncmp:
 	; Configure stack frame
-	push rbp            ; Guarda el valor de rbp en la pila
-	mov rbp, rsp        ; Establece rbp como el puntero de marco actual
-	mov rcx, 0          ; Inicializa rcx (contador) a cero
+	push rbp            ; Save the rbp value on the stack
+	mov rbp, rsp        ; Set rbp as the current frame pointer
+	mov rcx, 0          ; Initialize rcx (counter) to zero
 
 compare:
-	xor rax, rax        ; Limpia rax (se usará para el resultado)
-	xor rbx, rbx        ; Limpia rbx (se usará para comparar los bytes)
-	cmp ecx, edx        ; Compara el contador con la longitud n
-	je end_comparison   ; Si son iguales, finaliza la comparación
+	; Clear rax and rbx registers for use
+	xor rax, rax        ; Clean rax (will be used for the result)
+	xor rbx, rbx        ; Clean rbx (will be used to compare bytes)
 
-	mov al, byte [rdi + rcx]  ; Carga el byte de s1 en AL
-	mov bl, byte [rsi + rcx]  ; Carga el byte de s2 en BL
+	; Compare counter to length n
+	cmp ecx, edx        ; Compare counter to length n
+	je end_comparison   ; If they are equal, the comparison ends
+
+	; Load the byte of s1 into AL and the byte of s2 into BL
+	mov al, byte [rdi + rcx]  ; Load the byte of s1 into AL
+	mov bl, byte [rsi + rcx]  ; Load the byte of s2 into BL
+
+	; Compare the bytes
 	cmp al, bl           ; Compara los bytes
-	je equal_chars       ; Si son iguales, salta a la etiqueta equal
 
-	jl less_chars        ; Si no son iguales y al es menor que bl, salta a less
-	jg greater_chars     ; Si no son iguales y al es mayor que bl, salta a greater
+	; Jump to different tags depending on the comparison results
+	je equal_chars       ; If they are equal, jump to the equal tag
+	jl less_chars        ; If they are not equal and al is less than bl, jump to less
+	jg greater_chars     ; If they are not equal and al is greater than bl, jump to greater
 
 equal_chars:
 	; The characters are the same, check if it is a null character
-	cmp al, 0            ; Comprueba si hemos llegado al final de una de las cadenas
-	je end_comparison    ; Si es así, finaliza la comparación
-	inc rcx              ; Incrementa el contador
-	jmp compare          ; Vuelve a la etiqueta compare para comparar el siguiente byte
+	cmp al, 0            ; Checks if we have reached end of one of the chains
+	je end_comparison    ; If so, end the comparison
+	inc rcx              ; Increase the counter
+	jmp compare          ; Return to the compare tag to compare the next byte
 
 less_chars:
 	; Return a negative value (s1 < s2)
-	mov rax, -1          ; Si al es menor que bl, establece el resultado en -1 (menor)
-	jmp end_comparison   ; Salta a end_comparison para finalizar la comparación
+	mov rax, -1          ; If al is less than bl, set the result to -1 (smaller)
+	jmp end_comparison   ; Jump to end_comparison to end the comparison
 
 greater_chars:
 	; Return a positive value (s1 > s2)
-	mov rax, 1           ; Si al es mayor que bl, establece el resultado en 1 (mayor)
-	jmp end_comparison   ; Salta a end_comparison para finalizar la comparación
+	mov rax, 1           ; If al is greater than bl, set the result to 1 (greater)
+	jmp end_comparison   ; Jump to end_comparison to end the comparison
 
 end_comparison:
 	; clear stack frame
-	mov rsp, rbp         ; Restaura rsp al valor original
-	pop rbp              ; Restaura rbp desde la pila
-	ret                  ; Retorna con el resultado en rax
+	mov rsp, rbp         ; Restore rsp to original value
+	pop rbp              ; Restore rbp from stack
+	ret                  ; Returns with the result in rax
 
