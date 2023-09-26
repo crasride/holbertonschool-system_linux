75509c1b8e798e8c812fa91e75118ebceb707778
crasride <jose.dfernandez@yahoo.fr>
2023-09-26 17:42:55 +0200

    test

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -1,46 +1,33 @@
-section .text
 global asm_strncmp
+section .text
 
 asm_strncmp:
-	; Function prologue (stack frame setup)
-	push    rbp
-	mov     rbp, rsp
-
-	; Parameters:
-	; rdi = s1
-	; rsi = s2
-	; rdx = n
-
-	; Initialize counter
-	mov     rcx, rdx
-	
-compare_loop:
-	; Load characters from s1 and s2 into AL and BL
-	lodsb
-	mov     al, [rdi]
-	mov     bl, [rsi]
-
-	; Compare AL and BL
-	cmp     al, bl
-	jne     not_equal
-
-	; Check if we've reached the end of the strings or the end of the comparison
-	test    al, al
-	jz      equal
-	loop    compare_loop
-
+	push rbp
+	mov rbp, rsp
+	mov rcx, 0
+compare:
+	xor rax, rax
+	xor rbx, rbx
+	cmp ecx, edx
+	je exit
+	mov al, byte [rdi + rcx]
+	mov bl, byte [rsi + rcx]
+	cmp al, bl
+	je equal
+	jl less
+	jg greater
 equal:
-	; Strings are equal
-	xor     rax, rax
-	jmp     end
-
-not_equal:
-	; Strings are not equal
-	sub     al, bl
-	jmp     end
-
-end:
-	; Function epilogue (stack frame cleanup)
-	pop     rbp
+	cmp al, 0
+	je exit
+	inc rcx
+	jmp compare
+less:
+	mov rax, -1
+	jmp exit
+greater:
+	mov rax, 1
+	jmp exit
+exit:
+	mov rsp, rbp
+	pop rbp
 	ret
-
