01d3970aa3ebe63820117aea26b7a5920ba07742
crasride <jose.dfernandez@yahoo.fr>
2023-09-27 13:43:01 +0200

    new version

--- libasm/4-strstr.asm
+++ libasm/4-strstr.asm
@@ -1,41 +1,72 @@
-section .text
-global asm_strstr
+BITS 64
 
+	global asm_strstr
+
+	section .text
 asm_strstr:
-    ; Input:
-    ;   RDI - Pointer to the haystack (string to search in)
-    ;   RSI - Pointer to the needle (substring to find)
-    ; Output:
-    ;   RAX - Pointer to the first occurrence of 'needle' in 'haystack'
+    ;
+    push rbp
+    mov rbp, rsp
+    push rcx
+    push rbx
+    push rdx
+    push r8
+    push r9
 
-    xor rax, rax                  ;
-    cmp byte [rsi], 0             ;
-    je .found_needle              ;
+    ; Inicializar registros
+    xor rax, rax
+    xor rcx, rcx
+    xor r8, r8
+    xor r9, r9
 
-    .outer_loop:
-        mov rcx, rdi              ;
-        .inner_loop:
-            mov al, [rcx]         ;
-            mov dl, [rsi]         ;
-            cmp dl, 0             ;
-            je .found_needle_loop ;
-            cmp al, dl            ;
-            jne .next_char        ;
-            inc rcx               ;
-            inc rsi               ;
-            jmp .inner_loop       ;
+.search_loop:
+    ; Copiar el índice actual en RDX
+    mov rdx, rcx
+    xor rbx, rbx
 
-        .next_char:
-            inc rdi               ;
-            cmp byte [rdi], 0     ;
-            jne .outer_loop       ;
-            xor rax, rax          ;
-            ret
+.search_match:
+    ; Cargar el carácter actual de haystack en R8B
+    ; Cargar el carácter actual de needle en R9B
+    mov r8b, [rdi + rcx]
+    mov r9b, [rsi + rbx]
 
-        .found_needle_loop:
-            lea rax, [rdi - 1]    ;
-            ret
+    ; Comprobar si hemos llegado al final de needle (carácter nulo)
+    cmp r9b, 0
+    jz .found_needle
 
-    .found_needle:
-        lea rax, [rdi]             ;
-        ret
+    ; Comparar el carácter actual de haystack con el de needle
+    cmp r8b, r9b
+    jne .search_break
+
+    ; Incrementar índices y continuar buscando
+    inc rcx
+    inc rbx
+    jmp .search_match
+
+.search_break:
+    ; Cargar el siguiente carácter en haystack
+    mov r8b, [rdi + rcx]
+
+    ; Comprobar si es el final de haystack (carácter nulo)
+    cmp r8b, 0
+    jz .search_end
+
+    ; Incrementar índice en haystack y continuar buscando
+    inc rcx
+    jmp .search_loop
+
+.found_needle:
+    ; Calcular la dirección del inicio de needle en haystack
+    mov rax, rdi
+    add rax, rdx
+
+.search_end:
+    ;
+    pop r9
+    pop r8
+    pop rdx
+    pop rbx
+    pop rcx
+    mov rsp, rbp
+    pop rbp
+    ret
