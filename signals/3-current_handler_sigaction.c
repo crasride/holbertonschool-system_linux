f143d05b1945162f9616ab59ee01423b5f60e688
crasride <jose.dfernandez@yahoo.fr>
2023-10-09 15:56:02 +0200

    task 3 add comment fix betty

--- signals/3-current_handler_sigaction.c
+++ signals/3-current_handler_sigaction.c
@@ -1,5 +1,13 @@
 #include "signals.h"
 
+/**
+* current_handler_sigaction - Gets and returns the current signal handler for
+* SIGINT using seguiraction.
+*
+* Return: A pointer to the current signal handling function for SIGINT
+* If the current handle cannot be obtained or if it is an error, NULL is
+* returned.
+*/
 void (*current_handler_sigaction(void))(int)
 {
 	struct sigaction current_action;
