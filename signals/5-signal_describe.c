abdd7faf221e8f45287ba46076c61f39681768d5
crasride <jose.dfernandez@yahoo.fr>
2023-10-09 16:48:14 +0200

    task 5

---
+++ signals/5-signal_describe.c
@@ -0,0 +1,31 @@
+#include "signals.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Usage: %s <signum>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	int signum = atoi(argv[1]);
+
+	if (signum >= 1 && signum <= 64)
+	{
+		const char *signal_name = sys_siglist[signum];
+		if (signal_name != NULL)
+		{
+			printf("%d: %s\n", signum, signal_name);
+		}
+		else
+		{
+			printf("%d: Unknown signal %d\n", signum, signum);
+		}
+	}
+	else
+	{
+		printf("%d: Unknown signal %d\n", signum, signum);
+	}
+
+	return EXIT_SUCCESS;
+}
