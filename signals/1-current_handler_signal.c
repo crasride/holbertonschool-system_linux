9d91b0321bad456d29e714bf563a9602e7fd09a5
crasride <jose.dfernandez@yahoo.fr>
2023-10-09 14:41:39 +0200

    add comment and fix betty

--- signals/1-current_handler_signal.c
+++ signals/1-current_handler_signal.c
@@ -1,11 +1,21 @@
-#include <signal.h>
 #include "signals.h"
 
+/**
+* current_handler_signal - Gets and returns the current signal handler
+* for the SIGINT signal.
+*
+* Return: Un puntero a la función de manejo de señal actual para SIGINT.
+* If the current handle cannot be obtained or if it is an error, NULL is
+* returned.
+*/
 void (*current_handler_signal(void))(int)
 {
+/* Gets the current handler for SIGINT and stores it in 'handler'.*/
 	void (*handler)(int) = signal(SIGINT, SIG_DFL);
+
+/* Restores the original SIGIN handler*/
 	signal(SIGINT, handler);
-	return (handler == SIG_ERR) ? NULL : handler;
+
+/* Returns the current handler or NULL if an error occurred.*/
+	return ((handler == SIG_ERR) ? NULL : handler);
 }
-
-
