729ae1c79a69b3f04c580e9beb18db3a73dbe835
crasride <jose.dfernandez@yahoo.fr>
2023-10-09 13:46:32 +0200

    task0

--- signals/0-handle_signal.c
+++ signals/0-handle_signal.c
@@ -1,1 +1,22 @@
 #include "signals.h"
+
+/**
+* sigint_handler - Handler function for SIGINT
+* @signum: The signal number received (should be SIGINT)
+*/
+void sigint_handler(int signum)
+{
+	(void)signum;
+	printf("Gotcha! [%d]\n", signum);
+}
+
+/**
+* handle_signal - Sets a handler for the SIGINT signal
+* Return: 0 on success, -1 on error
+*/
+int handle_signal(void)
+{
+	if (signal(SIGINT, sigint_handler) == SIG_ERR)
+		return (-1);
+	return (0);
+}
