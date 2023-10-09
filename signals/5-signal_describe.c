2c22e460d55b44e7ff479234652a40ae396ac511
crasride <jose.dfernandez@yahoo.fr>
2023-10-09 16:55:24 +0200

    task 5 add commnet and fix betty error

--- signals/5-signal_describe.c
+++ signals/5-signal_describe.c
@@ -1,12 +1,21 @@
 #include "signals.h"
 
+/**
+* main - Displays the name of a signal based on its signal number.
+*
+* @argc: Number of command line arguments.
+* @argv: Array of character strings containing command line arguments.
+* Return: EXIT_SUCCESS if the operation is successful, EXIT_FAILURE in case of
+* error or incorrect arguments.
+*/
 int main(int argc, char *argv[])
 {
 	int signum;
+
 	if (argc != 2)
 	{
 		printf("Usage: %s <signum>\n", argv[0]);
-		return EXIT_FAILURE;
+		return (EXIT_FAILURE);
 	}
 
 	signum = atoi(argv[1]);
@@ -14,6 +23,7 @@ int main(int argc, char *argv[])
 	if (signum >= 1 && signum <= 64)
 	{
 		const char *signal_name = sys_siglist[signum];
+
 		if (signal_name != NULL)
 		{
 			printf("%d: %s\n", signum, signal_name);
@@ -28,5 +38,5 @@ int main(int argc, char *argv[])
 		printf("%d: Unknown signal %d\n", signum, signum);
 	}
 
-	return EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
 }
