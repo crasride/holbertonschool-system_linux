49e3c3842d0e5046e8547107022fa59cea2caaf5
crasride <jose.dfernandez@yahoo.fr>
2023-11-06 11:56:21 +0100

    fix betty

--- nm_objdump/hnm_32_solaris.c
+++ nm_objdump/hnm_32_solaris.c
@@ -6,14 +6,14 @@
 #include <elf.h>
 #include "hnm.h"
 
-void process_symbols_32bit_solaris(Elf32_Ehdr *ehdr, void *map, const char *filename)
+void process_symbols_32bit_solaris(Elf32_Ehdr *ehdr, void *map,
+									const char *filename)
 {
 		int i;
 		Elf32_Sym *symtab;
 		char *strtab_data;
 		int num_symbols;
 		Elf32_Shdr *shdr = (Elf32_Shdr *)((char *)map + ehdr->e_shoff);
-
 		Elf32_Shdr *symtab_section = NULL;
 		Elf32_Shdr *strtab_section = NULL;
 
@@ -37,7 +37,6 @@ void process_symbols_32bit_solaris(Elf32_Ehdr *ehdr, void *map, const char *file
 
 		symtab = (Elf32_Sym *)((char *)map + symtab_section->sh_offset);
 		strtab_data = (char *)((char *)map + strtab_section->sh_offset);
-
 		num_symbols = symtab_section->sh_size / sizeof(Elf32_Sym);
 
 		for (i = 0; i < num_symbols; i++)
@@ -45,13 +44,16 @@ void process_symbols_32bit_solaris(Elf32_Ehdr *ehdr, void *map, const char *file
 			if (symtab[i].st_name != 0)
 			{
 				char *symbol_name = strtab_data + symtab[i].st_name;
+
 				printf("%08x %s\n", symtab[i].st_value, symbol_name);
 			}
 		}
 	}
+	/* &(elf->shstrtab[elf->shdr[i].sh_name]) */
 
 
-int analyze_32bit_elf_solaris(Elf32_Ehdr *ehdr, void *map, const char *filename)
+int analyze_32bit_elf_solaris(Elf32_Ehdr *ehdr, void *map,
+								const char *filename)
 {
 	if (ehdr->e_ident[EI_DATA] == ELFDATA2LSB)
 	{
