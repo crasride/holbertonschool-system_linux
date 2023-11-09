3aed6133684879876db0f168595131e7c0ffd9a3
crasride <jose.dfernandez@yahoo.fr>
2023-11-09 15:40:53 +0100

    print section 64-3

--- nm_objdump/hobjdump.c
+++ nm_objdump/hobjdump.c
@@ -270,7 +270,7 @@ void print_section_contents_64(Elf64_Shdr *shdr, char *map, int is_big_endian)
 
 	for (i = 0; i < section_size; i += 16)
 	{
-		printf(" %x", (int)(my_be32toh(shdr->sh_addr, is_big_endian) + i));
+		printf("%04x", (int)(my_be32toh(shdr->sh_addr, is_big_endian) + i));
 		for (j = 0; j < 16; j++)
 		{
 			if (i + j < section_size)
@@ -336,6 +336,7 @@ void print_sections_64(Elf64_Ehdr *ehdr, int is_big_endian, void *map)
 			strcmp(section_name, ".tm_clone_table") == 0 ||/* solaris */
 			strcmp(section_name, ".rel.text") == 0 ||
 			strcmp(section_name, ".rel.data") == 0 ||
+
 			strcmp(section_name, ".strtab") == 0)
 			{
 			continue;
