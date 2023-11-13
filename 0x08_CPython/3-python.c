5b1974f39228893acd3125c56d701c480a374b99
crasride <jose.dfernandez@yahoo.fr>
2023-11-13 19:14:13 +0100

    modify printf

--- 0x08_CPython/3-python.c
+++ 0x08_CPython/3-python.c
@@ -15,7 +15,6 @@ void print_python_float(PyObject *p);
 */
 void print_python_list(PyObject *p)
 {
-	printf("[*] Python list info\n");
 	/* Check if the object is a valid List object */
 	if (!PyList_Check(p))
 	{
