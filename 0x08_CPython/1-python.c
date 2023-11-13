652d9e563817e92211989d1e20edbd5327bf70bb
crasride <jose.dfernandez@yahoo.fr>
2023-11-13 11:05:15 +0100

    task 1

--- 0x08_CPython/1-python.c
+++ 0x08_CPython/1-python.c
@@ -1,25 +1,28 @@
 #include <Python.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void print_python_list(PyObject *p)
 {
+
     PyListObject *list = (PyListObject *)p;
     Py_ssize_t size, alloc, i;
     PyObject *item;
 
+
     size = PyList_Size(p);
     alloc = list->allocated;
 
-    printf("[*] Información de la lista de Python\n");
-    printf("[*] Tamaño de la lista de Python = %zd\n", size);
-    printf("[*] Asignado = %zd\n", alloc);
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %zd\n", size);
+    printf("[*] Allocated = %zd\n", alloc);
+
 
     for (i = 0; i < size; i++)
     {
         item = PyList_GetItem(p, i);
-        printf("Elemento %zd: %s\n", i, Py_TYPE(item)->tp_name);
+        printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
     }
 }
-
 
 
