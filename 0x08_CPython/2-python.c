6f24030583096628bed7ff02d6fff56ab6dc3a40
crasride <jose.dfernandez@yahoo.fr>
2023-11-13 15:51:50 +0100

    fix betty and comment

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -1,24 +1,38 @@
 #include <Python.h>
 #include <stdio.h>
 
+
+/**
+* print_python_bytes - Print information about a Python Bytes object.
+*
+* This function prints information about a Python Bytes object, including its
+* size and the first 10 bytes in hexadecimal format.
+*
+* @p: A pointer to the Python Bytes object.
+*/
 void print_python_bytes(PyObject *p)
 {
+	/* pointer to store byte string */
 	char *byte_string = NULL;
+	/* Variable to store the size of the byte string */
 	Py_ssize_t byte_size;
 
 	printf("[.] bytes object info\n");
 
+	/* Check the object is a valid Bytes object */
 	if (!PyBytes_Check(p))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
+	/* Retrieve byte string and its size */
 	PyBytes_AsStringAndSize(p, &byte_string, &byte_size);
 	printf("  size: %zd\n", byte_size);
 	printf("  trying string: %s\n", byte_string);
 	printf("  first %zd bytes:", byte_size <= 10 ? byte_size + 1 : 10);
 
+	/* Iterate through the bytes to print in hexadecimal */
 	for (int i = 0; i <= byte_size && i < 10; i++)
 	{
 		printf(" %02hhx", byte_string[i]);
@@ -27,6 +41,14 @@ void print_python_bytes(PyObject *p)
 }
 
 
+/**
+* print_python_list - Print information about a Python List object.
+*
+* This function prints information about a Python List object, including its
+* size, allocation, and the types of its elements.
+*
+* @p: A pointer to the Python List object.
+*/
 void print_python_list(PyObject *p)
 {
 	/* Check if the object is a valid List object */
