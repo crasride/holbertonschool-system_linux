0d95feaeecf66048e09014cbc2925de18a74c063
crasride <jose.dfernandez@yahoo.fr>
2023-11-14 09:17:07 +0100

    fix betty c90 define

--- 0x08_CPython/5-python.c
+++ 0x08_CPython/5-python.c
@@ -1,5 +1,8 @@
 #include <Python.h>
 
+#define BASE_SHIFT 30
+#define CALCULATE_BASE() (1UL << BASE_SHIFT)
+
 /**
 * print_python_int - Display a PyLong Object's value.
 * @p: A pointer to the PyObject.
@@ -13,9 +16,9 @@ void print_python_int(PyObject *p)
 	Py_ssize_t size = long_obj->ob_base.ob_size;
 	Py_ssize_t i;
 	unsigned long result = 0;
-
 	/* uses base 2^30 for digits */
-	unsigned long base = (unsigned long)1 << 30;
+	/* unsigned long base = (unsigned long)1 << 30; */
+	unsigned long base = CALCULATE_BASE();
 
 	/* Check the given object is valid Python integer */
 	if (!PyLong_Check(p))
@@ -23,7 +26,6 @@ void print_python_int(PyObject *p)
 		printf("Invalid Int Object\n");
 		return;
 	}
-
 	/*
 	* If the size is negative, it's a negative integer; print the minus sign
 	* and make size positive
@@ -33,19 +35,16 @@ void print_python_int(PyObject *p)
 		printf("-");
 		size = -size;
 	}
-
 	/* Check for C unsigned long int overflow */
 	if (size > 3 || (size == 3 && long_obj->ob_digit[2] > 15))
 	{
 		printf("C unsigned long int overflow\n");
 		return;
 	}
-
 	for (i = size - 1; i >= 0; i--)
 	{
 		result = result * base + long_obj->ob_digit[i];
 	}
-
 	printf("%lu\n", result);
 }
 
