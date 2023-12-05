e044dd3705fbf238525045d30159c8d91b729a4b
crasride <jose.dfernandez@yahoo.fr>
2023-12-05 15:11:24 +0100

    task0

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -1,54 +1,43 @@
 #include "strace.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
+	pid_t child_pid;
+	int status, syscall_number, print_next_syscall;
+	struct user_regs_struct user_registers;
 
-	pid_t child;
-	int status;
-	long orig_rax;
-	struct user_regs_struct regs;
+	setbuf(stdout, 0);
+	child_pid = fork();
 
-	if (argc < 2)
+	if (child_pid < 0)
+		exit(EXIT_FAILURE);
+	else if (child_pid == 0)
 	{
-		fprintf(stderr, "Usage: %s command [args...]\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-	/* Create a child process */
-	child = fork();
-
-	if (child == -1) {
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	if (child == 0) {
-		/* Child process */
-		if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1) {
-			perror("ptrace");
-			exit(EXIT_FAILURE);
-		}
-		execvp(argv[1], &argv[1]);
-		perror("execvp");
-		exit(EXIT_FAILURE);
+		ptrace(PTRACE_TRACEME, 0, 0, 0);
+		raise(SIGSTOP);
+		argv[argc] = NULL;
+		execvp(argv[1], argv + 1);
 	}
 	else
 	{
-		/* Parent process */
-		waitpid(child, &status, 0);
+		wait(&status);
+		ptrace(PTRACE_SYSCALL, child_pid, 0, 0);
 
-		while (WIFSTOPPED(status))
+		for (print_next_syscall = 0; !WIFEXITED(status); print_next_syscall ^= 1)
 		{
-			/* Get the system call number */
-			ptrace(PTRACE_GETREGS, child, NULL, &regs);
-			orig_rax = regs.orig_rax;
+			wait(&status);
+			ptrace(PTRACE_GETREGS, child_pid, 0, &user_registers);
 
-			/* Print the system call number */
-			printf("%ld\n", orig_rax);
+			if (print_next_syscall)
+			{
+				syscall_number = user_registers.orig_rax;
+				printf("%d\n", syscall_number);
+			}
 
-			/* Continue the execution of the child process */
-			ptrace(PTRACE_SYSCALL, child, NULL, NULL);
-			waitpid(child, &status, 0);
+			ptrace(PTRACE_SYSCALL, child_pid, 0, 0);
 		}
 	}
 
 	return (0);
 }
+
