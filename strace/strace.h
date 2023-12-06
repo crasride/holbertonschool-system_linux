f61538d307c4ee1938bba0399b43c317332860e3
crasride <jose.dfernandez@yahoo.fr>
2023-12-06 16:33:21 +0100

    add functions

--- strace/strace.h
+++ strace/strace.h
@@ -6,12 +6,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/ptrace.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/user.h>
-#include <sys/reg.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <syscall.h>
+#include <sys/ptrace.h>
+#include <sys/reg.h>
+
+#define ENOSYS_ERROR -38
+
+/*Functions fors Task 1 */
+void print_syscall_name(struct user_regs_struct regs);
+void trace_child(pid_t child);
+int execute_traced_command(char *argv[], char *envp[]);
+
+/*Functions fors Task 2 and 3 */
+void print_params(struct user_regs_struct *regs);
+void traceSyscalls(pid_t child_pid);
+pid_t createTracedProcess(char **argv);
+
+
 
 #endif /* __STRACE_H__ */
