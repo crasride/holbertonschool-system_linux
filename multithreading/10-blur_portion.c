10fb6881adf993c17cc66fd961a360873bdc98ce
crasride <jose.dfernandez@yahoo.fr>
2024-01-02 11:32:05 +0100

    modify

--- multithreading/10-blur_portion.c
+++ multithreading/10-blur_portion.c
@@ -1,62 +1,60 @@
 #include "multithreading.h"
 
-static void accumulate_color(const img_t *img, const kernel_t *kernel,
-							size_t x, size_t y,
-							float *sum_r, float *sum_g, float *sum_b)
+static void accumulate_color(blur_portion_t const *portion, size_t dest_row,
+							size_t dest_col, float *sum_r, float *sum_g,
+							float *sum_b)
 {
-	size_t radius;
+	size_t kernel_half_size = portion->kernel->size / 2;
 	size_t kernel_row, kernel_col;
-	int img_row, img_col;
 
 	*sum_r = 0.0;
 	*sum_g = 0.0;
 	*sum_b = 0.0;
 
-	radius = kernel->size / 2;
-
-	for (kernel_row = 0; kernel_row < kernel->size; kernel_row++)
+	for (kernel_row = 0; kernel_row < portion->kernel->size; kernel_row++)
 	{
-		for (kernel_col = 0; kernel_col < kernel->size; kernel_col++)
+		for (kernel_col = 0; kernel_col < portion->kernel->size; kernel_col++)
 		{
-			img_row = (int)(y - radius + kernel_row);
-			img_col = (int)(x - radius + kernel_col);
+			int img_row = (int)(dest_row - kernel_half_size + kernel_row);
+			int img_col = (int)(dest_col - kernel_half_size + kernel_col);
 
-			if (img_row >= 0 && img_row < (int)img->h &&
-				img_col >= 0 && img_col < (int)img->w)
+			if (img_col >= 0 && img_col < (int)portion->img->w &&
+				img_row >= 0 && img_row < (int)portion->img->h)
 			{
-				float kernel_value = kernel->matrix[kernel_row][kernel_col];
-				*sum_r += kernel_value * img->pixels[(size_t)img_row * img->w + (size_t)img_col].r;
-				*sum_g += kernel_value * img->pixels[(size_t)img_row * img->w + (size_t)img_col].g;
-				*sum_b += kernel_value * img->pixels[(size_t)img_row * img->w + (size_t)img_col].b;
+				float kernel_value = portion->kernel->matrix[kernel_row][kernel_col];
+				*sum_r += kernel_value * portion->img->pixels[img_row * portion->img->w + img_col].r;
+				*sum_g += kernel_value * portion->img->pixels[img_row * portion->img->w + img_col].g;
+				*sum_b += kernel_value * portion->img->pixels[img_row * portion->img->w + img_col].b;
 			}
 		}
 	}
 }
 
-void blur_portion(const blur_portion_t *portion)
+void blur_portion(blur_portion_t const *portion)
 {
 	size_t dest_row, dest_col;
-	size_t img_blur_index = 0;
-
-	if (!portion || !portion->img || !portion->img_blur || !portion->kernel)
-		return;
+	float sum_r, sum_g, sum_b;
+	size_t img_blur_index;
 
 	for (dest_row = 0; dest_row < portion->h; dest_row++)
 	{
 		for (dest_col = 0; dest_col < portion->w; dest_col++)
 		{
-			float sum_r, sum_g, sum_b;
-			accumulate_color(portion->img, portion->kernel,
-							portion->x + dest_col, portion->y + dest_row,
-							&sum_r, &sum_g, &sum_b);
+			accumulate_color(portion, dest_row, dest_col, &sum_r, &sum_g, &sum_b);
 
 			img_blur_index = (portion->y + dest_row) * portion->img_blur->w + (portion->x + dest_col);
-			portion->img_blur->pixels[img_blur_index].r = (uint8_t)(sum_r);
-			portion->img_blur->pixels[img_blur_index].g = (uint8_t)(sum_g);
-			portion->img_blur->pixels[img_blur_index].b = (uint8_t)(sum_b);
+
+			if (dest_col < portion->img_blur->w && dest_row < portion->img_blur->h)
+			{
+				portion->img_blur->pixels[img_blur_index].r = (uint8_t)sum_r;
+				portion->img_blur->pixels[img_blur_index].g = (uint8_t)sum_g;
+				portion->img_blur->pixels[img_blur_index].b = (uint8_t)sum_b;
+			}
 		}
 	}
 }
+
+
 
 
 
