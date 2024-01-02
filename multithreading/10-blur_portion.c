2fe21fc032351a37af7be156d5c85c777e0caf52
crasride <jose.dfernandez@yahoo.fr>
2024-01-02 13:51:27 +0100

    modify function

--- multithreading/10-blur_portion.c
+++ multithreading/10-blur_portion.c
@@ -4,14 +4,16 @@
 void perform_bluring(const img_t *img, img_t *new_img, const kernel_t *kernel,
 					size_t x, size_t y)
 {
-	float divider = 0, total_r = 0, total_g = 0, total_b = 0;
+	size_t i = 0, j = 0;
 	size_t new_img_index = 0, half_kernel_size = 0;
+	float divider = 0, total_r = 0, total_g = 0, total_b = 0;
+
 
 	half_kernel_size = kernel->size / 2;
 
-	for (size_t i = y > half_kernel_size ? y - half_kernel_size : 0; i < (y + half_kernel_size < img->h ? y + half_kernel_size : img->h); i++)
+	for (i = y > half_kernel_size ? y - half_kernel_size : 0; i < (y + half_kernel_size < img->h ? y + half_kernel_size : img->h); i++)
 	{
-		for (size_t j = x > half_kernel_size ? x - half_kernel_size : 0; j < (x + half_kernel_size < img->w ? x + half_kernel_size : img->w); j++)
+		for (j = x > half_kernel_size ? x - half_kernel_size : 0; j < (x + half_kernel_size < img->w ? x + half_kernel_size : img->w); j++)
 		{
 			size_t img_row = i;
 			size_t img_col = j;
@@ -34,12 +36,14 @@ void perform_bluring(const img_t *img, img_t *new_img, const kernel_t *kernel,
 
 void blur_portion(blur_portion_t const *portion)
 {
+	size_t x = 0, y = 0;
+
 	if (!portion || !portion->img || !portion->img_blur || !portion->kernel)
 		return;
 
-	for (size_t y = portion->y; y < portion->y + portion->h; y++)
+	for (y = portion->y; y < portion->y + portion->h; y++)
 	{
-		for (size_t x = portion->x; x < portion->x + portion->w; x++)
+		for (x = portion->x; x < portion->x + portion->w; x++)
 		{
 			perform_bluring(portion->img, portion->img_blur, portion->kernel, x, y);
 		}
