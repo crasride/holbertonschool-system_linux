8238506d6108670e6e138be31d6c37ff38510124
crasride <jose.dfernandez@yahoo.fr>
2024-02-12 15:21:49 +0100

    task 8

--- sockets/socket.h
+++ sockets/socket.h
@@ -15,25 +15,6 @@
 
 #include <ctype.h>
 
-/**
-* struct addrinfo - structure to store address information
-* @ai_family: the address family for the new socket
-* @ai_socktype: the type of socket
-* @ai_protocol: the protocol for the new socket
-* @ai_addrlen: the length of the socket address
-* @ai_addr: the socket address
-* @ai_next: pointer to the next node in the linked list
-*/
-struct addrinfo
-{
-	int ai_family;
-	int ai_socktype;
-	int ai_protocol;
-	socklen_t ai_addrlen;
-	struct sockaddr *ai_addr;
-	struct addrinfo *ai_next;
-	};
-
 int setup_server_socket(void);
 int accept_cli_connection(int server_socket, struct sockaddr_in *client_addr,
 							socklen_t *client_addr_len);
